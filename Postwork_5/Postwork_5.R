a6b3e35596210f9fb17be35565bf07bec0406ec9
omar17md <86132736+omar17md@users.noreply.github.com>
2021-07-06 12:54:14 -0600

    Create Postwork_5.R

---
+++ Postwork_5/Postwork_5.R
@@ -0,0 +1,55 @@
+#1. A partir del conjunto de datos de soccer de la liga española de las temporadas 
+#2017/2018, 2018/2019 y 2019/2020, crea el data frame SmallData, que contenga las 
+#columnas date, home.team, home.score, away.team y away.score; esto lo puede hacer 
+#con ayuda de la función select del paquete dplyr. Luego establece un directorio de 
+#trabajo y con ayuda de la función write.csv guarda el data frame como un archivo csv 
+#con nombre soccer.csv. Puedes colocar como argumento row.names = FALSE en write.csv.
+
+#Establecemos la ruta del directorio
+setwd("D:/Omar/Programacion/Codigos/R/Bedu/dataframe")
+
+#Exportamos la biblioteca dplyr para utilizar algunas funciones
+library(dplyr)
+
+#Creamos una lista a partir los 3 dataframes de las 3 temporadas
+lista <- lapply(dir(), read.csv)
+
+#Seleccionamos las columnas que queremos de la lista y lo guardamos en SmallData
+SmallData = lapply(lista ,select, c("Date", "HomeTeam", "FTHG", "AwayTeam" ,"FTAG"))
+
+SmallData = do.call(rbind, SmallData)
+
+colnames(SmallData) = c("date", "home.team", "home.score", "away.team", "away.score")
+
+SmallData$date =gsub("/17", "/2017", SmallData$date)
+SmallData$date =gsub("/18", "/2018", SmallData$date)
+
+
+write.csv(SmallData, file="soccer.csv", row.names = F)
+
+
+library(fbRanks)
+
+listasoccer <- create.fbRanks.dataframes(scores.file = 'soccer.csv', date.format = "%d/%m/%Y")
+
+anotaciones = listasoccer$scores
+  
+equipos = listasoccer$teams
+  
+fecha = unique(listasoccer$scores$date)
+
+n = length(fecha)
+
+ranking = rank.teams(anotaciones, equipos, max.date = fecha[n-1], min.date = fecha[1], date.format = "%Y-%m-%d")
+
+prediccion = predict.fbRanks(ranking, date = fecha[n])
+
+originales = SmallData[SmallData$date == format(fecha[n], "%d/%m/%Y"),]
+
+resultados =  prediccion[[1]][c("home.score","away.score")]
+colnames(resultados) = c("predic.home.score", "predic.away.score")
+
+comparacion = cbind(originales, resultados)
+
+comparacion <- subset(comparacion, select=c("date","home.team", "home.score", 
+                                            "predic.home.score", "away.team", "away.score", "predic.away.score"))
