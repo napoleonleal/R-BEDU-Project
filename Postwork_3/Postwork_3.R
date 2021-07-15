924c3cdce711ae160951ad31c8cc8196328abc1a
omar17md <86132736+omar17md@users.noreply.github.com>
2021-07-14 21:10:50 -0600

    Update Postwork_3.R

--- Postwork_3/Postwork_3.R
+++ Postwork_3/Postwork_3.R
@@ -1,65 +1,126 @@
-#POSTWORK 3
+### POSTWORK 3
+# Importamos las bibliotecas:
 library(ggplot2)
-#Ahora generaremos gráficas con la tabla que se generó en el postwork 2
-
-#La probabilidad (marginal) de que el equipo que juega en casa anote x goles (x=0,1,2,)
-goles.local <- table(data$FTHG)
-goles.visitante <- table(data$FTAG)
-
-(total_partidos <- length(data$FTHG)) #total de partidos
-(pm.local <- round((goles.local/total_partidos)*100,2)) #prob marginal 
-(goles.local <- data.frame(goles.local, pm.local)[-3]) 
-colnames(goles.local)=c("Goles","Frecuencia", "Prob.Marginal")
-goles.local
-
-#Un gráfico de barras para las probabilidades marginales estimadas del 
-#número de goles que anota el equipo de casa.
-ggplot(goles.local, aes(x = Goles, y = pm.local)) + geom_bar(stat="identity")
-
-#La probabilidad marginal de que el equipo que juega como visitante 
-#anote y goles 
-(pm.vis <- round((goles.visitante/total_partidos)*100,2))
-goles.visitante <- data.frame(goles.visitante, pm.vis)[-3]
-colnames(goles.visitante)=c("Goles","Freq", "Prob.Marginal")
-goles.visitante
-
-#Un gráfico de barras para las probabilidades marginales estimadas del número 
-#de goles que anota el equipo visitante.
-ggplot(goles.visitante, aes(x = Goles, y = Pmg)) + geom_bar(stat="identity")
-
-#La probabilidad conjunta de que el equipo que juega en casa anote x goles y 
-#el equipo que juega como visitante anote y goles (x=0,1,2,, y=0,1,2,)
-goles.partidos = table(data$FTHG, data$FTAG, dnn=c("x", "y"))
-(prob.conjunta = prop.table(goles.partidos))
-(prob.conjunta <- round(prob.conjunta * 100,2))
-
-#Un HeatMap para las probabilidades conjuntas estimadas de los números de 
-#goles que anotan el equipo de casa y el equipo visitante en un partido.
-str(prob.conjunta)
-
-library(lattice)
 library(plotly)
+library(dplyr)
+library(ggplot2)
+library(viridis)
 library(viridisLite)
-library(grid)
 
-#Utilizando lattice se hace el heat map
-(heat.map <- as.matrix(prob.conjunta) )# volvemos la tabla una matriz
-inter <-levelplot(heat.map, col.regions = viridis(100))
+# Con el data frame obtenido realizaremos algunas gráficas.
+data <- "https://raw.githubusercontent.com/napoleonleal/R-BEDU-Project/main/Postwork_2/Postwork_02.csv" %>%
+        read.csv %>% 
+        mutate(Date = as.Date(Date, "%Y-%m-%d"))
 
-#agregando ejes y título
-(heat.map <- as.matrix(prob.conjunta) )
-levelplot(heat.map, col.regions = viridis(100), xlab= "goles local", ylab="goles invitado", main = "Mapa de calor de las probabilidades conjuntas", colorkey )
-trellis.focus("legend", side="right", clipp.off=TRUE, highlight=FALSE)
-grid.text("probabilidad [%]", 0.4, 1.1, hjust=0.5, vjust=1.5)
-trellis.unfocus()
+### Local: Probabilidad Marginal
 
-#Para utilizar plotly y volverlo interactivo  convertimos la matriz a df
-heat.df <- as.data.frame(t(heat.map))
-heat.df
+# Calcularemos la probabilidad marginal de que el equipo local anote ’x’ goles mediante una función previa:
+prob.m.local <- 
+  get.prob.df(data, FTHG, "Gol.Home", "P.Marginal") 
 
-p <- ggplot(heat.df, aes(x, y, fill= Freq)) +  #gráfica
-  geom_raster() +
-  scale_fill_viridis_c()
+(prob.m.local %>% get.round(3))
 
-ggploty(p) 
+# Realizamos una función de una gráfica para vizualizar los datos:
+plot.bar <- function(data, x.lab, y.lab, f.lab, title){
+  Goles         <- data[1] %>% unlist()
+  Porcentaje    <- (data[3]*100 ) %>% unlist %>% round(., digits=2)
+  Prob.Marginal <- data[3] %>% unlist() %>% round(., digits=4)
+  data %>%
+  ggplot() +
+  geom_bar(stat = 'identity') +
+  aes(  x = Goles
+      , y = Porcentaje
+      , fill = Porcentaje
+      , text = paste("Prob Marginal", Prob.Marginal)
+                      #, group = interaction(Goles, Porcentaje)
+       ) +
+  labs(  x     = x.lab
+       , y     = y.lab
+       , fill  = f.lab
+       , title = title
+       ) +
+  theme_minimal() +
+  theme(  text = element_text(size=15)
+        , legend.title = element_text(size=10) 
+        ) +
+  scale_fill_viridis(name=f.lab, direction = 1) 
+}
+
+# Realizamos una gráfica para vizualizar los datos:
+plot.local <- plot.bar(prob.m.local
+              , "Goles [n]"
+              , "Probabilidad de ocurrencia [%]"
+              , "%"
+              , "Probabilidad de anotación del equipo local"
+              )
+
+# Local: P(x) Marginal
+ggplotly(plot.local) #versión interactiva
+
+
+### Visitante: Probabilidad Marginal
+
+# Ahora calcularemos la probabilidad para el equipo visitante:
+prob.m.visit <- 
+  get.prob.df(data, FTAG, "Gol.Away", "P.Marginal") 
+ 
+(prob.m.visit %>% get.round(3))
+
+# Realizamos una gráfica para vizualizar los datos:
+plot.visit <- plot.bar(prob.m.visit
+              , "Goles [n]"
+              , "Probabilidad de ocurrencia [%]"
+              , "%"
+              , "Probabilidad de anotación del equipo visitante"
+              )
+
+# Visitante: P(y) Marginal
+ggplotly(plot.visit) #versión interactiva
+
+### Probabilidad Conjunta P(x∩y)
+
+# La probabilidad conjunta de que el equipo que juega en casa anote ‘x’ goles
+# y el equipo que juega como visitante anote ’y’ goles calculada con una función previa:
+prob.joint <- 
+  get.prob.joint.tbl(data, data$FTHG, data$FTAG, "Local", "Visitante") 
+
+(prob.joint  %<>% "*"(100) %>% round(2))
+
+# Realizamos un heat map con una función para visualizar los datos:
+prob.joint %<>% as.data.frame() %>% rename(Probabilidad = Freq)
+
+# Declaramos una funcion para graficar la probabilidad conjunta
+plot.heatmap <- function(data, x.lab, y.lab, f.lab, title){
+  Local         <- data[1] %>% unlist
+  Visitante     <- data[2] %>% unlist
+  Probabilidad  <- data[3] %>% unlist
+    ggplot(data) +
+    aes(   Local
+         , Visitante
+         , fill = Probabilidad
+         ) + #gráficamos
+    geom_raster() +
+    labs(  x     = x.lab
+         , y     = y.lab
+         , fill  = f.lab
+         , title = title
+         ) +
+    theme(  text = element_text(size=18)
+          , legend.title = element_text(size=15) 
+          ) +
+    scale_fill_viridis(  name=f.lab
+                       , direction = 1 #, option = "H"
+                       ) 
+}
+
+# Realizamos una gráfica para visualizar los datos:
+plot.mapa.calor <-
+plot.heatmap(  prob.joint
+             , "Local [goles]"
+             , "Visitante [goles]"
+             , "Probabilidad [%]"
+             , "Probabilidad Conjunta de anotación"
+             )
+# Graficamos el heatmap
+ggplotly(plot.mapa.calor)    #versión interactiva
 
